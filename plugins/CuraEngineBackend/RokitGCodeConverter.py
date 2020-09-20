c7cfc69e2cc810de2fc5482b06537df279b79634
jungjuseong <jungjuseong@gmail.com>
2020-09-20 18:09:46 +0900

    find T in range 1~5

--- plugins/CuraEngineBackend/RokitGCodeConverter.py
+++ plugins/CuraEngineBackend/RokitGCodeConverter.py
@@ -218,7 +218,7 @@ class RokitGCodeConverter:
                     uvcode = self._getUVCode(self._current_index, self._current_layer - 1)                    
                     if uvcode != '':
                         before_layer_uvcode = self._P.getWrappedUVCode(uvcode, self._current_index, self._current_layer_height)
-                        for j in range(5):
+                        for j in range(1,5):
                             if gcode_list[j].startswith('T'):
                                 have_just_next_extruder = True # danger op because we expect gcode list ;LAYER ...T0 
                 continue
