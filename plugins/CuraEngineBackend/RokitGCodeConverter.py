f74e990d1e297636018dcd8480a961dcde8ee5be
jungjuseong <jungjuseong@gmail.com>
2020-10-21 19:03:46 +0900

    add Tool setup code

--- plugins/CuraEngineBackend/RokitGCodeConverter.py
+++ plugins/CuraEngineBackend/RokitGCodeConverter.py
@@ -177,10 +177,14 @@ class RokitGCodeConverter:
                          
                 if self._previous_tool != self._current_tool: # tool changed
                     self._logical_layer = self._real_layer - self._tool_initial_layers[self._previous_tool]
-                    uvcode = self._P.getUVCode(self._previous_tool, self._logical_layer, self._real_layer)                    
+                    uvcode = self._P.getUVCode(self._previous_tool, self._logical_layer, self._real_layer)  
+                    bed_pos = self._P.getBedPos(self._current_tool)
+                    reset_height = (self._G['G0_Z40_C30_F420'] + self._G['M29_B']) if self._current_tool > 0 else ''
+
                     if uvcode != '' and self._before_layer_use_uv == False:
-                        bed_pos = self._P.getBedPos(self._current_tool) + ';(TOOL_CHANGED)\n'
                         gcode_list[self._tool_index] = uvcode + bed_pos + gcode_list[self._tool_index]
+                    else:
+                        gcode_list[self._tool_index] = reset_height + bed_pos + gcode_list[self._tool_index]
                     self._before_layer_use_uv = False
                     self._previous_tool = self._current_tool
                 else: # layer changed
